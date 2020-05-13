aa5c87ba14d850f3782b77c84e7794db23d4bd53
Uriel <ubonano@gmail.com>
2020-05-12 23:39:50 -0300

    addWorkshopBloc implemented

--- lib/screens/workshops/widgets/add_workshop_fab/bloc/bloc/add_workshop_bloc.dart
+++ lib/screens/workshops/widgets/add_workshop_fab/bloc/bloc/add_workshop_bloc.dart
@@ -1,12 +1,21 @@
 import 'dart:async';
-
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:equatable/equatable.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:bt_habitat_ft_organizacion/models/workshop_model.dart';
+import 'package:bt_habitat_ft_organizacion/repositories/workshops_repository.dart';
 
 part 'add_workshop_event.dart';
 part 'add_workshop_state.dart';
 
 class AddWorkshopBloc extends Bloc<AddWorkshopEvent, AddWorkshopState> {
+  final WorkshopsRepository _workshopsRepository;
+
+  AddWorkshopBloc({@required WorkshopsRepository workshopsRepository})
+      : assert(workshopsRepository != null),
+        _workshopsRepository = workshopsRepository;
+
   @override
   AddWorkshopState get initialState => AddWorkshopInitial();
 
@@ -14,6 +23,26 @@ class AddWorkshopBloc extends Bloc<AddWorkshopEvent, AddWorkshopState> {
   Stream<AddWorkshopState> mapEventToState(
     AddWorkshopEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AddWorkshopStarted) {
+      yield* _mapAddWorkshopStartedToState(event);
+    } else if (event is AddWorkshopFinished) {
+      yield* _mapAddWorkshopFinishedToState(event);
+    }
+  }
+
+  Stream<AddWorkshopState> _mapAddWorkshopStartedToState(AddWorkshopStarted event) async* {
+    yield AddWorkshopInProgress();
+    _workshopsRepository
+        .addNewWorkshop(event.workshop)
+        .then((value) => add(AddWorkshopFinished(true, "¡Taller creado con exito!")))
+        .catchError((error) => add(AddWorkshopFinished(false, "Ocurrio un problema, intente mas tarde")));
+  }
+
+  Stream<AddWorkshopState> _mapAddWorkshopFinishedToState(AddWorkshopFinished event) async* {
+    if(event.wasAdded){
+      yield AddWorkshopSuccess(event.message);
+    } else {
+      yield AddWorkshopFailure(event.message);
+    }
   }
 }
--- lib/screens/workshops/widgets/add_workshop_fab/bloc/bloc/add_workshop_state.dart
+++ lib/screens/workshops/widgets/add_workshop_fab/bloc/bloc/add_workshop_state.dart
@@ -8,3 +8,37 @@ class AddWorkshopInitial extends AddWorkshopState {
   @override
   List<Object> get props => [];
 }
+
+class AddWorkshopInProgress extends AddWorkshopState {
+  @override
+  List<Object> get props => [];
+
+  @override
+  String toString() => 'State: AddWorkshopInProgress';
+}
+
+class AddWorkshopSuccess extends AddWorkshopState {
+  
+  final String message;
+
+  AddWorkshopSuccess(this.message);
+
+  @override
+  List<Object> get props => [this.message];
+  
+  @override
+  String toString() => 'State: AddWorkshopSucces - Message: ${this.message}';
+}
+
+class AddWorkshopFailure extends AddWorkshopState {
+
+  final String message;
+
+  AddWorkshopFailure(this.message);
+
+  @override
+  List<Object> get props => [this.message];
+
+  @override
+  String toString() => 'State: AddWorkshopFailure - Message: ${this.message}';
+}
